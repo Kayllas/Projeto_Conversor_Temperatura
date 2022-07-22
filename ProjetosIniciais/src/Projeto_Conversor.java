03dfdb7fe1ead03d85e048c220809f01179334d9
Kayque Castro <kayqueflavionel@hotmail.com>
2022-07-22 13:56:49 -0300

    Alteração de espaçamento.

--- ProjetosIniciais/src/Projeto_Conversor.java
+++ ProjetosIniciais/src/Projeto_Conversor.java
@@ -1,3 +1,4 @@
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -10,10 +11,10 @@ public class Projeto_Conversor {
 
         char resp;
         do {
-            System.out.print("Digite a temperatura em Celsius" );
+            System.out.print("Digite a temperatura em Celsius " );
             double C = sc.nextDouble();
             double F = 9.0 * C / 5.0 + 32.0;
-            System.out.printf("Equivalente em Fahrenheit: %.1f%n", F);
+            System.out.printf("Equivalente em Fahrenheit: %.1f%n ", F);
             System.out.print("deseja repetir (s/n)? ");
             resp = sc.next().charAt(0);
         } while (resp != 'n');
